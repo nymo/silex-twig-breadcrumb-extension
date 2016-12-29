995557e3430a48c020fb3f797967e03bd426095d
nymo <gp@gregorpanek.de>
2016-12-29 21:48:24 +0100

    Added initial testsuite with first test case

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file is part of silex-twig-breadcrumb-extension
+ *
+ * (c) Gregor Panek <gp@gregorpanek.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
